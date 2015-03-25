835bf8e2137330ec5fd4a35ae88c72bd8533a559
Michael Sparks <sparks.m@gmail.com>
2015-03-25 21:27:13 +0000

    Decorate literals with source information

--- pyxie/parsing/grammar.py
+++ pyxie/parsing/grammar.py
@@ -22,15 +22,42 @@ class Grammar(object):
         p[0] = [ "statements", [ p[1] ] + p[2][1] ]
 
     def p_statement_1(self, p):
-        """statement : NUMBER EOL
-                     | FLOAT EOL
-                     | HEX EOL
-                     | OCTAL EOL
-                     | BINARY EOL
-                     | STRING EOL
-                     | BOOLEAN EOL
-                     """
+        "statement : value_literal EOL"
         p[0] = [ "statement", p[1] ]
+
+
+
+    def p_value_literal_1(self, p):
+        "value_literal : NUMBER"
+        p[0] = [ "value_literal", p[1], "NUMBER", "INT", p.lineno(1) ]
+
+    def p_value_literal_2(self, p):
+        "value_literal : FLOAT"
+        p[0] = [ "value_literal", p[1], "FLOAT", p.lineno(1) ]
+
+    def p_value_literal_3(self, p):
+        "value_literal : HEX"
+        p[0] = [ "value_literal", p[1], "NUMBER", "HEX", p.lineno(1) ]
+
+    def p_value_literal_4(self, p):
+        "value_literal : OCTAL"
+        p[0] = [ "value_literal", p[1], "NUMBER", "OCTAL", p.lineno(1) ]
+
+    def p_value_literal_5(self, p):
+        "value_literal : BINARY"
+        p[0] = [ "value_literal", p[1], "NUMBER", "BINARY", p.lineno(1) ]
+
+    def p_value_literal_6(self, p):
+        "value_literal : STRING"
+        p[0] = [ "value_literal", p[1], "STRING", p.lineno(1) ]
+
+    def p_value_literal_7(self, p):
+        "value_literal : BOOLEAN"
+        p[0] = [ "value_literal", p[1], "BOOLEAN", "INT", p.lineno(1) ]
+
+
+
+
 
 def parse(source,lexer):
    yacc.yacc(module=Grammar())
