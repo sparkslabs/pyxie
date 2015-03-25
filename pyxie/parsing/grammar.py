33fd4bd50d6a4b93f0270f2a42d768aca2dd297a
Michael Sparks <sparks.m@gmail.com>
2015-03-25 20:42:47 +0000

    Initial value literal based parser

---
+++ pyxie/parsing/grammar.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import ply.yacc as yacc
+
+from pyxie.parsing.lexer import tokens
+
+class Grammar(object):
+    tokens = tokens
+    def p_error(self,p):
+        print "Syntax error at", p
+
+    def p_program(self, p):
+        "program : statements"
+        p[0] = [ "statements", p[1] ]
+
+    def p_statements_1(self, p):
+        "statements : statement"
+        p[0] = [ "statements", p[1] ]
+
+    def p_statements_2(self, p):
+        "statements : statement statements"
+        p[0] = [ "statements", p[1], p[2] ]
+
+    def p_statement_1(self, p):
+        """statement : NUMBER EOL
+                     | FLOAT EOL
+                     | HEX EOL
+                     | OCTAL EOL
+                     | BINARY EOL
+                     | STRING EOL
+                     | BOOLEAN EOL
+                     """
+        p[0] = [ "statement", p[1] ]
+
+def parse(source,lexer):
+   yacc.yacc(module=Grammar())
+   result = yacc.parse(source, lexer=lexer)
+   return result
