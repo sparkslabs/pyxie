ba6727e6b34bf3a18aad06d83662284e6f3e9ba5
Michael Sparks <sparks.m@gmail.com>
2015-03-29 17:39:13 +0100

    Represent C programs as json, and allow construction from json

--- pyxie/codegen/simple_cpp.py
+++ pyxie/codegen/simple_cpp.py
@@ -7,10 +7,38 @@ def Print(*args):
     y = " ".join([str(x) for x in args])
     source.append(y)
 
+def mkStatement(statement_spec):
+    ss = statement_spec
+    if ss[0] == "assigment":
+        return Assigment( ss[1], ss[3], ss[2])
+
+    if ss[0] == "print_statement":
+        return PrintStatement(*statement_spec[1:])
+
+
 class C_Program(object):
     def __init__(self):
         self.includes = []
         self.main_cframe = C_Frame()
+        self.name = ""
+
+    @classmethod
+    def fromjson(klass, json):
+        program = klass()
+        prog_desc = json["PROGRAM"]
+        program.name = prog_desc["name"]
+        program.includes = list(prog_desc["includes"])
+        main_spec = prog_desc["main"]["c_frame"]
+        for identifier in main_spec["identifiers"]:
+            print "IDENTIFIERS"
+            program.main_cframe.identifiers.append(Identifier(identifier[1], identifier[2]))
+
+        for statement in main_spec["statements"]:
+            conc_statement = mkStatement(statement)
+            program.main_cframe.statements.append(conc_statement)
+
+
+        return program
 
     def generate(self):
         frame_lines = self.main_cframe.concrete()
@@ -27,10 +55,23 @@ class C_Program(object):
         Print("    "+ "return 0;")
         Print("}")
 
+    def json(self):
+        return { "PROGRAM": {"name": self.name,
+                             "includes" : self.includes,
+                             "main": self.main_cframe.json()}
+               }
+
 class C_Frame(object):
     def __init__(self):
         self.identifiers = []
         self.statements = []
+
+    def json(self):
+        for y in self.statements:
+            print y
+        return {"c_frame": {"identifiers" : [ x.json() for x in self.identifiers ],
+                            "statements" : [y.json() for y in self.statements ] }
+               }
 
     def concrete(self):
         block = []
@@ -40,8 +81,9 @@ class C_Frame(object):
 
         block.append(blank_line)
         for statement in self.statements:
-            code = statement.code()
-            block.append(code + ";")
+            if statement:
+                code = statement.code()
+                block.append(code + ";")
         return block
 
 class Identifier(object):
@@ -49,20 +91,30 @@ class Identifier(object):
         self.ctype = ctype
         self.name = name
 
+    def json(self):
+        return ["identifier", self.ctype, self.name ]
+
     def decl_code(self):
         return self.ctype + " " + self.name
 
 class Assigment(object):
-    def __init__(self, lvalue, rvalue):
+    def __init__(self, lvalue, rvalue, assigntype="="):
         self.lvalue = lvalue
         self.rvalue = rvalue
+        self.assigntype = assigntype
+
+    def json(self):
+        return ["assigment", self.lvalue, self.assigntype, self.rvalue ]
 
     def code(self):
-        return self.lvalue + " = " + self.rvalue
+        return self.lvalue + " "+self.assigntype+" " + self.rvalue
 
 class PrintStatement(object):
     def __init__(self, *args):
         self.args = args
+
+    def json(self):
+        return ["print_statement" ] + list(self.args[:])
 
     def code(self):
         return "cout << " + " << \" \" << ".join(self.args) + " << endl"
@@ -71,6 +123,9 @@ makefile_tmpl = """
 all :
 	g++ %(filename)s.c -o %(filename)s
 """
+
+def build_program(json):
+    json = C_Program.fromjson(json)
 
 
 if __name__ == "__main__":
@@ -90,13 +145,27 @@ if __name__ == "__main__":
     main.statements.append(Assigment("name", '"world"'))
     main.statements.append(PrintStatement("greeting", "name"))
 
+    import pprint
+
+    progj = program.json()
+
+    pprint.pprint(progj)
+
+    program_clone = C_Program.fromjson(progj)
+    progj2 = program_clone.json()
+
+    program = program_clone
+
+    print "--------------------------------------------------------------"
+    pprint.pprint(progj2)
+
     program.generate()
 
     import time
     import pprint
     import os
 
-    pprint.pprint(source)
+    pprint.pprint(source, width=200)
     now = int(time.time())
     dirname = str(now - 1427000000)
     print "BUILDING PROGRAM", dirname
