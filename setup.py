d6be7891771e96cf45dafbe447bf20fb25822dad
Michael Sparks <sparks.m@gmail.com>
2015-04-22 19:34:53 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ packages = find_packages(".")
 package_names = packages.keys()
 
 setup(name = "pyxie",
-      version = "0.0.3",
+      version = "0.0.4",
       description = "Little Python to C++ Compiler",
       url='http://www.sparkslabs.com/michael/',
       author='Michael Sparks (sparkslabs)',
